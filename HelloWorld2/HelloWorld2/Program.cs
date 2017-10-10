ae7bf8ceaff93adf0954aaefff28bbc8a46e02de
ArthurMartinez001 <arthurmartinez001@gmail.com>
2017-10-10 15:49:02 -0700

    Variables

--- HelloWorld2/HelloWorld2/Program.cs
+++ HelloWorld2/HelloWorld2/Program.cs
@@ -7,6 +7,7 @@ namespace HelloWorld2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello I'm in my own World");
+            Console.WriteLine("do it");∫√ 
         }
     }
 }
---
+++ VariablesDemo/VariablesDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VariablesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 21; //not true
+            int x = 5;
+            double myHourRate = 150.00;
+            var myName = "Arthur";
+            Console.WriteLine($"myAge: {myAge}, x: {x}, myHourlyRate:{myHourRate}, myName: {myName}");
+
+
+            // this part of the lesson, the user is able to figure out how to change all the original values
+            myAge = 99;
+            x = 100;
+            myHourRate = 200.00;
+            myName = "Silver Surfer";
+            Console.WriteLine($"myAge: {myAge}, x: {x}, myHourlyRate:{myHourRate}, myName: {myName}");
+
+                
+        }
+    }
+}
