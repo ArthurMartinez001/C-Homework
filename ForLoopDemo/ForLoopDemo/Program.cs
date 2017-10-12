1016f62d3335e295991a6d84e45883e5256f2bb0
ArthurMartinez001 <arthurmartinez001@gmail.com>
2017-10-12 12:51:15 -0700

    For Loop Homework

---
+++ ForLoopDemo/ForLoopDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ForLoopDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, increase numbers");
+            for (int i = 0; i < 300; i++) // the ++ increaments the value of i by 1
+            {
+                Console.WriteLine($"Increase: {i.ToString()}");
+                // in the section i.ToString() <-- that converts the numbers into a readeable string. 
+                //also note that i.ToString is a Method and it is uisng these brackets {}
+            }
+            Console.WriteLine("Goodbye, decrease numbers");
+            for (int d = 300; d > 0; d--)
+                //I wrote this to show how to decrease from 300 to 1
+                //notice the d- - decreases the number so d is greater than 0
+            {
+                Console.WriteLine($"Decrease: {d.ToString()}");
+            }
+        }
+    }
+}
